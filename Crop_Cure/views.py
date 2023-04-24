f756ef9777bef4a0d95ad411a700ea435b12785c
Govind <govvedi@gmail.com>
2023-04-24 23:07:35 +0530

    New Result page added

--- Crop_Cure/views.py
+++ Crop_Cure/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from PIL import Image
-import cv2
+from django.conf import settings
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import img_to_array
 from tensorflow.keras.applications.vgg19 import preprocess_input
@@ -9,27 +9,27 @@ import numpy as np
 model=load_model("best_model.h5")
 
 result = {
-  0 : "Apple_Apple_scab",
+  0 : "Apple_scab",
   1 : "Apple_Black_rot",
-  2 :  "Apple_Cedar_apple_rust",
+  2 : "Apple_Cedar_apple_rust",
   3 : "Apple_healthy",
-  4 : "Corn_cercospora_leaf_spot_Gray_leaf_spot",
+  4 : "Corn_cercospora_leaf_spot_gray_leaf_spot",
   5 : "Corn_Common_rust",
-  6 : "Corn_healthy",
-  7 : "Corn_Northern_leaf_Blight",
+  6 : "Corn_Northern_leaf_Blight",
+  7 : "Corn_healthy",
   8 : "Potato_Early_blight",
-  9 : "Potato_healthy",
-  10 : "Potato_Late_blight",
+  9 : "Potato_Late_blight",
+  10 : "Potato_healthy",
   11 : "Tomato_Bacterial_spot",
   12 : "Tomato_early_blight",
-  13 : "Tomato_healthy",
-  14 : "Tomato_Late_blight",
-  15 : "Tomato_Leaf_mold",
-  16 : "Tomato_septoria_leaf_spot",
-  17 : "Tomato_spider_mites Two-spotted_spider_mite",
-  18 : "Tomato_target_spot",
+  13 : "Tomato_Late_blight",
+  14 : "Tomato_Leaf_mold",
+  15 : "Tomato_septoria_leaf_spot",
+  16 : "Tomato_spider_mites Two-spotted_spider_mite",
+  17 : "Tomato_target_spot",
+  18 : "Tomato_yellow_leaf_curl_virus",
   19 : "Tomato_mosaic_virus",
-  20 : "Tomato_tomato_yellow_leaf_curl_virus"
+  20 : "Tomato_healthy"
 }
 
 def home(request):
@@ -42,27 +42,26 @@ def upload(request):
       img = request.FILES['img']
       imag = Image.open(img)              #Converts input image to PIL
 
-      open_cv_image = np.array(imag)
-      open_cv_image = open_cv_image[:, :, ::-1].copy()    #Convert PIL to cv2
-      cv2.imshow("hi",open_cv_image)
+      img_path = settings.MEDIA_ROOT + "\\input.jpeg"
 
-      imag = cv2.resize(open_cv_image, (256, 256))      #Resize cv2 image
-      print("Final = ",imag)
-      cv2.imshow('image', imag)
+      if imag.format=="PNG":              #If imge is PNG, it converts to JPEG
+        imag = imag.convert("RGB")
+        imag.save(img_path, "jpeg")
 
-      color_coverted = cv2.cvtColor(imag, cv2.COLOR_BGR2RGB)
-      pil_image = Image.fromarray(color_coverted)       #Converts resized cv2 image again to PIL
-      pil_image.show()
+      iz = imag.resize((256,256))
+      iz.save(img_path, "jpeg")
 
-      imagR=img_to_array(pil_image)
-
+      imagR=img_to_array(iz)
       im=preprocess_input(imagR)
       img=np.expand_dims(im,axis=0)
       img=img.reshape(1,256,256,3)
+      
       pred=np.argmax(model.predict(img))
+
       context = {
         'pred' : result[pred],
         }
+      return render(request, 'result.html', context)
   return render(request, 'upload.html', context)
 
 def supplements(request):
